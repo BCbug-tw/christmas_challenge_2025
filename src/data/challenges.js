29a5e65b9be3b0c1caf2ca40d8defc9dc01aa16e
BC <bchuangtw2k@gmail.com>
2025-12-12 11:56:20 +0800

    feat: add challenges data file

--- src/data/challenges.js
+++ src/data/challenges.js
@@ -1,9 +1,9 @@
 export const challengeImages = [
   // 您可以在這裡新增或修改您的照片網址
   // 格式： '網址',
-  'https://images.unsplash.com/photo-1516961642265-531546e84af2?ixlib=rb-4.0.3', // 範例：情侶喝咖啡
-  'https://images.unsplash.com/photo-1529333166437-7750a6dd5a70?ixlib=rb-4.0.3', // 範例：背影
-  'https://images.unsplash.com/photo-1621609764095-6c23d6a7e67c?ixlib=rb-4.0.3', // 範例：牽手
-  'https://images.unsplash.com/photo-1542038784456-1ea0e93ca370?ixlib=rb-4.0.3', // 範例：互拍
-  'https://images.unsplash.com/photo-1516575972752-198d09e7addd?ixlib=rb-4.0.3', // 範例：跳舞
+  'https://pin.it/41FQw1p5E',
+  'https://pin.it/1cdpRImHl',
+  'https://pin.it/AY7Fi1gTU',
+  'https://pin.it/2fJG1MIjH',
+  'https://pin.it/2hEZNceTF',
 ];
